9554d71fb6a9e1578c2cbdd1a1816a9a2904bae0
Ahmet Sivri <ahmet.sivri@cleverspeck.com>
2018-08-23 16:24:38 -0400

    Typicode fake rest api implemented

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import DataCollection from 'data-collection'
 
 Vue.use(Vuex)
 
@@ -21,17 +22,21 @@ export const actions = {
     //--Below this part of the code communicates with an API to reach for TODOs--
     fetchTodos: (context) =>
     {
-        axios.get("URL")
+        axios.get("http://jsonplaceholder.typicode.com/todos")
         .then(response =>{
             console.log(response.data);
-            context.commit("FETCH_TODO", response.data)
+            var manipulated = new DataCollection(response.data);
+            var names = manipulated.query().distinct('title'); // todos is a unique element in the json response. 
+            names.forEach(element =>{
+                context.commit("FETCH_TODO", element)
+            })
         }).catch(error =>{
             console.error(error);
         })
     },
     postTodo: (context, todoP) =>
     {
-        axios.post("URL", {
+        axios.post("http://localhost:64516/api/Todos", {
             todos: todoP,
             completed: false
         })
